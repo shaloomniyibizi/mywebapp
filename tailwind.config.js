b1a712ce43b45ae82519a6ae8f1f46a34de85050
Shaloom Niyibizi <shaloomniyibizi@gmal.com>
2023-05-21 14:33:08 +0200

    first fixed commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,38 @@
+
+
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`
+    }
+    return `rgb(var(${variableName}))`
+  }
+}
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [],
+	content: ["./view/**/*.{html,js}"],
 	theme: {
     extend: {
+      textColor: {
+        skin: {
+          base: withOpacity('--color-text-base'),
+          muted: withOpacity('--color-text-muted'),
+          inverted: withOpacity('--color-text-inverted'),
+        },
+      },
+      backgroundColor: {
+        skin: {
+          fill: withOpacity('--color-fill'),
+          'button-accent': withOpacity('--color-button-accent'),
+          'button-accent-hover': withOpacity('--color-button-accent-hover'),
+          'button-muted': withOpacity('--color-button-muted'),
+        },
+      },
+      gradientColorStops: {
+        skin: {
+          hue: withOpacity('--color-fill'),
+        },
+      },
       keyframes: {
         wiggle: {
           '0%, 100%': { transform: 'rotate(-3deg)' },
